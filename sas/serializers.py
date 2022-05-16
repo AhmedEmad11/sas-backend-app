0464d0c36df114e9271ebed8e319a3404a92cd6b
Ahmed Emad <ahmeddemadd11@gmail.com>
2022-05-16 21:18:11 +0200

    fixed attendance serializer

--- sas/serializers.py
+++ sas/serializers.py
@@ -16,20 +16,22 @@ class SubjectSerializer(serializers.ModelSerializer):
 class AttendanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attendance
-        fields = ['id', 'studentId', 'subjectId', 'count', 'username', 'firstname']
+        fields = ['id', 'studentId', 'subjectId', 'count', 'username', 'firstname', 'lastname']
     username = serializers.CharField(source='studentId.username')
-    firstname = serializers.CharField(source='studentId')
+    firstname = serializers.CharField(source='studentId.first_name')
+    lastname = serializers.CharField(source='studentId.last_name')
         
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
-        fields = ['id', 'username', 'role', 'level']
+        fields = ['id', 'username', 'firstname', 'lastname', 'role', 'level']
     username= serializers.CharField(source="user.username")
+    firstname= serializers.CharField(source="user.first_name")
+    lastname= serializers.CharField(source="user.last_name")
         
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'firstname', 'lastname', 'role', 'levelName', 'levelId']
+        fields = ['id', 'username', 'first_name', 'last_name', 'role']
     role = serializers.CharField(source='role.role')
-    levelName = serializers.CharField(source='role.level.name')
-    levelId = serializers.CharField(source='role.level.id')
+    
