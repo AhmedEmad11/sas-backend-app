a9e44dbdfc287c7ffbdc5bad707a7685d17681e8
Ahmed Emad <ahmeddemadd11@gmail.com>
2024-05-10 17:47:47 +0300

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-
+import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sas.urls'))
